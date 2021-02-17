6068aade7d184f42f701e25fd9980407a25855f9
abmarie <77949813+abmarie@users.noreply.github.com>
2021-02-17 13:29:41 -0800

    all buttons working

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -4,9 +4,14 @@ FSJS Project 2 - Data Pagination and Filtering
 */
 
 let itemsPerPage=9; //defaults display to nine students per page
+const pagination = document.querySelector('.pagination');
+const studentList = document.querySelector('.student-list');
 
 //showPage function creates elements of one students and inserts itemsPerPage students onto a page
 function showPage (list, page) {
+  if ((page*itemsPerPage)>list.length) {
+    page=(list.length/itemsPerPage);
+  }
   const startIndex = (page*itemsPerPage)-itemsPerPage;
   let endIndex = (page*itemsPerPage);
   //elimnates errors that from the list of items being not evenly divisible by the itemsPerPage variable.
@@ -14,7 +19,6 @@ function showPage (list, page) {
     endIndex=list.length;
   }
   //concatinates student data items into an html string & inserts string
-  const studentList = document.querySelector('.student-list');
   studentList.innerHTML='';
   for (let i=0; i<=list.length; i++) {
     if (i>= startIndex && i<endIndex) {
@@ -46,62 +50,75 @@ function showPage (list, page) {
 }
 }
 
+function addItemsPerPageButton() {
+  const addItemsPerPageButton = `
+    <ul class="itemsPerPage">
+    <li>
+      <div>
+      <button type="button" class="showmore">Show 18 Students Per Page</button>
+      </div>
+    </li>
+    </ul>`;
+  pagination.insertAdjacentHTML('beforeend',addItemsPerPageButton);
+}
 /*addPagination adds buttons for each page. the number of buttons is calculated by dividing the number of students by
 the number of students per page.*/
 function addPagination (list) {
-  const linkList = document.querySelector('.link-list');
-  const  pagination = document.querySelector('.pagination');
   const numberOfPaginationButtons = (list.length/itemsPerPage);
+
+  const linkList = document.querySelector('.link-list');
   linkList.innerHTML ='';
   for (let i=0; i<=numberOfPaginationButtons; i++) {
     let paginationButton = `
       <li>
-        <button type="button">${i+1}</button>
+        <button type="button" class = "inactive">${i+1}</button>
       </li>`;
       linkList.insertAdjacentHTML('beforeend',paginationButton);
   }
   //applies active formating to first page button
   const firstPaginationButton = document.querySelector('.link-list button:first-child');
   firstPaginationButton.className = 'active';
-
-  const addItemsPerPageButton = `
-    <ul class="itemsPerPage">
-      <li>
-        <div>
-        <button type="button" class="showmore">Show 18 Students Per Page</button>
-        </div>
-      </li>
-    </ul>`;
-      pagination.insertAdjacentHTML('beforeend',addItemsPerPageButton);
-  //listens for clicks on page buttons and calls the showPage function to display the next page of students
-  pagination.addEventListener ('click', (e) => {
-    const paginationButton = document.querySelectorAll('ul.link-list li button')
-    const activePaginationButtons = document.querySelector('.active');
-    if (e.target = paginationButton) {
-      activePaginationButtons.className = '';
-      e.target.className = 'active';
-      page = e.target.textContent;
-      showPage(list,page);
-      }
-    });
-  pagination.addEventListener ('click', (e) => {
-    const itemsPerPageButton = document.querySelector('.showmore')
-    if (e.target = itemsPerPageButton) {
-      let itemsPerPage = 18;
-      itemsPerPageButton.textContent = 'Show 9 Students Per Page';
-      itemsPerPageButton.className='showfewer'
-      showPage(list,page);
-          }
-        });
-    pagination.addEventListener ('click', (e) => {
-      const itemsPerPageButton = document.querySelector('.showfewer')
-      if (e.target = itemsPerPageButton) {
-        let itemsPerPage = 9;
-        itemsPerPageButton.textContent = 'Show 18 Students Per Page';
-        showPage(list,page);
-            }
-          });
 }
 
-showPage(data,1);
+  //listens for clicks on page buttons and calls the showPage function to display the next page of students
+pagination.addEventListener ('click', (e) => {
+  if (e.target.tagName ==='BUTTON') {
+  const button = e.target;
+  const activeButtons = document.querySelector('.active');
+  const list=data;
+  const action = button.className;
+  const nameActions = {
+    showmore: () => {
+      itemsPerPage = 18;
+      button.textContent = 'Show 9 Students Per Page';
+      button.className='showfewer';
+      page = activeButtons.textContent;
+      addPagination(list);
+      showPage(list,page);
+    },
+    showfewer: () => {
+        itemsPerPage = 9;
+        button.textContent = 'Show 18 Students Per Page';
+        button.className='showmore';
+        page = activeButtons.textContent;
+        addPagination(list);
+        showPage(list,page);
+    },
+    inactive: () => {
+        activeButtons.className='inactive';
+        button.className='active';
+        page = button.textContent;
+        showPage(list,page);
+    },
+    active: () => {
+      page = button.textContent;
+      showPage(list,page);
+    }
+  };
+  nameActions[action]();
+}
+});
+
 addPagination(data);
+addItemsPerPageButton();
+showPage(data,1);
